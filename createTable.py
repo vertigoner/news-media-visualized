f71438164285e69068d627a05e38bbb8d80e819a
Ashwin Natarajan <ashwinn3@gmail.com>
2019-03-25 13:52:39 -0400

    db stuff

---
+++ createTable.py
@@ -0,0 +1,39 @@
+import boto3
+
+# Get the service resource.
+dynamodb = boto3.resource('dynamodb',aws_access_key_id='AKIAJZ6F6DJHN7MYYPJQ', aws_secret_access_key='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', region_name='us-east-2')
+
+# Create the DynamoDB table.
+table = dynamodb.create_table(
+    TableName='sourceDistribution',
+    KeySchema=[
+        {
+            'AttributeName': 'keyword',
+            'KeyType': 'HASH'
+        },
+        {
+            'AttributeName': 'publications',
+            'KeyType': 'RANGE'
+        }
+    ],
+    AttributeDefinitions=[
+        {
+            'AttributeName': 'keyword',
+            'AttributeType': 'S'
+        },
+        {
+            'AttributeName': 'publications',
+            'AttributeType': 'S'
+        },
+    ],
+    ProvisionedThroughput={
+        'ReadCapacityUnits': 5,
+        'WriteCapacityUnits': 5
+    }
+)
+
+# Wait until the table exists.
+table.meta.client.get_waiter('table_exists').wait(TableName='sourceDistribution')
+
+# Print out some data about the table.
+print(table.item_count)
