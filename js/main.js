0280a0e6f992b12581ba5128159bf685b57d479d
vertigoner <nbroberts97@gmail.com>
2019-04-22 11:10:44 -0400

    some functionality accomplished

--- js/main.js
+++ js/main.js
@@ -14,7 +14,9 @@ const bubbleColor = "#428bca"
 const filterColor = "#d30b0d"
 const neutralColor = "lightgray";
 
-var selected = "";
+var trends = []
+
+var selectedBubble = null;
 
 var keywordEntry = function(keyword, articleCount) {
     this.keyword = keyword
@@ -35,27 +37,41 @@ function start() {
         .attr("width", width)
         .attr("height", height);
 
-    var radiusScale = d3.scale.linear()
+    var radiusScale1 = d3.scale.linear()
         .domain([1, 100])
         .range([10, 45]);
+
+    var radiusScale2 = d3.scale.linear()
+        .domain([1, 20])
+        .range([20, 75]);
 
     var searchButton = d3.select("#buttonWrapper")
         .append('button')
         .text('Search')
         .attr('class', 'button card')
         .on('click', function() {
+            let input = document.getElementById("query").value;
+            getJSON(`http://localhost:5000/getSourceDistribution/${input}`, function(err, data) {
+                if (err !== null) {
+                    alert('Something went wrong: ' + err);
+                    return;
+                }
 
-            console.log(document.getElementById("query").value);
+                data = cleanSourceData(data)
 
-
-
-            // getAggregate
-            data.push({
-                keyword: "test",
-                articleCount: 15
+                let entry = new keywordEntry(
+                    input,
+                    Object.values(data).reduce(function(acc, a) {
+                        return acc + a;
+                    })
+                )
+                if (!trends.some(function(el) { 
+                    return el.keyword === entry.keyword 
+                })) {
+                    trends.push(entry)
+                    restartTrends();
+                }
             });
-            
-            update();
         });
 
     // create trendBubbleChart 
@@ -67,31 +83,114 @@ function start() {
             return;
         }
 
-        dataArray = []
-        for (let key of Object.keys(data)) {
-            dataArray.push(new keywordEntry(key, data[key]))
+        trends = Object.keys(data).map(function(key) {
+            return new keywordEntry(key, data[key])
+        })
+
+        restartTrends();
+    });
+
+    trendBubbleChart.on("click", function() {
+        sourceBubbleChart.selectAll("g")
+            .data([])
+            .exit().remove();
+
+        if (this === d3.event.target) {
+            trendBubbleChart.selectAll("circle").attr("fill", bubbleColor);
+            selectedBubble = null;
+            return;
         }
 
+        selectedBubble = d3.select(d3.event.target.parentNode);
+        var bubbleData = selectedBubble.data()[0];
+
+        trendBubbleChart.selectAll("circle").attr("fill", neutralColor);
+        selectedBubble.select("circle").attr("fill", filterColor);
+
+        getJSON(`http://localhost:5000/getSourceDistribution/${bubbleData.keyword}`, function(err, data) {
+            if (err !== null) {
+                alert('Something went wrong: ' + err);
+                return;
+            }
+
+            data = cleanSourceData(data)
+            data = Object.keys(data).map(function(key) {
+                return {
+                    source: key,
+                    articleCount: data[key]
+                };
+            });
+            
+            var nodes = sourceBubbleChart.selectAll(".node")
+                .data(data)
+                .enter()
+    
+            nodes.append("g")
+                .attr("class", "node")
+                .append("circle")
+                .attr("r", function(d) {
+                    return radiusScale2(d.articleCount);
+                })
+                .attr("fill", bubbleColor)
+                
+            sourceBubbleChart.selectAll("g")
+                .append("text")
+                .attr("class", "unselectable")
+                .text(function(d) { return d.source; });
+
+            force = d3.layout.force() //set up force
+                .size([width, height])
+                .nodes(data)
+                .charge(-200)
+                .on("tick", function() {
+                    sourceBubbleChart.selectAll("circle")
+                        .attr("cx", function(d) {
+                            return d.x;
+                        }) 
+                        .attr("cy", function(d) {
+                            return d.y;
+                        });
+                    sourceBubbleChart.selectAll("text")
+                        .attr("x", function(d) {
+                            return d.x;
+                        }) 
+                        .attr("y", function(d) {
+                            return d.y + 3;
+                        });
+                })
+
+            force.start()
+        })
+    })
+
+    function restartTrends() {
         var nodes = trendBubbleChart.selectAll(".node")
-            .data(dataArray)
+            .data(trends)
             .enter()
     
         nodes.append("g")
             .attr("class", "node")
             .append("circle")
             .attr("r", function(d) {
-                return radiusScale(d.articleCount);
+                return radiusScale1(d.articleCount);
             })
-            .attr("fill", bubbleColor)
+            .attr("fill", function() {
+                console.log(selectedBubble)
+                if (selectedBubble == null) {
+                    return bubbleColor;
+                } else {
+                    return neutralColor;
+                }
+            })
             
         trendBubbleChart.selectAll("g")
             .append("text")
             .attr("class", "unselectable")
             .text(function(d) { return d.keyword; });
-
+    
         force = d3.layout.force() //set up force
             .size([width, height])
-            .nodes(dataArray)
+            .nodes(trends)
             .charge(-200)
             .on("tick", function() {
                 trendBubbleChart.selectAll("circle")
@@ -109,35 +208,13 @@ function start() {
                         return d.y + 3;
                     });
             })
-
+    
         force.start()
-    });
+    }
+}
 
-    trendBubbleChart.on("click", function() {
-        if (this === d3.event.target) {
-            trendBubbleChart.selectAll("circle").attr("fill", bubbleColor);
-            return;
-        }
-
-        trendBubbleChart.selectAll("circle").attr("fill", neutralColor);
-
-        var selectedBubble = d3.select(d3.event.target.parentNode);
-        var bubbleData = selectedBubble.data()[0];
-
-        selectedBubble.select("circle")
-            .attr("fill", filterColor);
-
-        getJSON(`http://localhost:5000/getSourceDistribution/${bubbleData.keyword}`, function(err, data) {
-            if (err !== null) {
-                alert('Something went wrong: ' + err);
-                return;
-            }
-            
-            for (let key of Object.keys(data.publications)) {
-                // console.log(key)
-            }
-        })
-    })
+function cleanSourceData(data) {
+    return JSON.parse(data.publications.replace(/u'|'/g,'"'))
 }
 
 
