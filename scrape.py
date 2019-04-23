7423d36145ddafac69f09767193f3ad97cffdd91
tmudireddy <thushara@gatech.edu>
2019-04-23 13:17:59 -0400

    new api key

--- scrape.py
+++ scrape.py
@@ -14,11 +14,11 @@
 #********************************NEWSAPI*******************************************
 import requests
 import json
-import db_util
+# import db_util
 from newsapi import NewsApiClient
 
 def read(keyword):
-    newsapi = NewsApiClient(api_key='51f9df310d85434c9fbbca122bc636bd')
+    newsapi = NewsApiClient(api_key='f6a59919c932489bb16c7a48f5f5f007')
     everything = newsapi.get_everything(q= keyword,from_param='2019-03-25',
                                           to='2019-04-21',
                                           language = 'en', page = 1, page_size = 100, sort_by = "popularity")
@@ -41,14 +41,15 @@ def read(keyword):
 
 def addSourceDistributions():
     keywords = ["GOP", "White House", "Democrats", "Republicans"]
-    # keywords = ['Mueller', 'Election 2020', 'Indian Elections', 'Bernie Sanders', 'Trump',
-    # 'Green New Deal', 'Biden', 'Cory Booker', 'Pete Buttigieg', 'Donald Trump', 'Russia', 'Sri Lanka', 'Notre Dame', 'Venezuela']
+    keywords = ['Mueller', 'Election 2020', 'Indian Elections', 'Bernie Sanders', 'Trump',
+    'Green New Deal', 'Biden', 'Cory Booker', 'Pete Buttigieg', 'Donald Trump', 'Russia', 'Sri Lanka', 'Notre Dame', 'Venezuela']
     for k in keywords:
-        sources, v = read(k)
-        db_util.insertJSON('sourceDistribution', v)
+        sources, v, count = read(k)
+        print(v)
+        # db_util.insertJSON('sourceDistribution', v)
 
 def top(keyword):
-    newsapi = NewsApiClient(api_key='51f9df310d85434c9fbbca122bc636bd')
+    newsapi = NewsApiClient(api_key='f6a59919c932489bb16c7a48f5f5f007')
     headlines = newsapi.get_top_headlines(q= keyword,
                                           language='en', page_size = 100, page = 1)
     articles = headlines["articles"]
@@ -65,12 +66,12 @@ def top(keyword):
 
 def addTopHeadlines():
 
-    # keywords = ['Mueller', 'Election 2020', 'Indian Elections', 'Bernie Sanders', 'Trump',
-    # 'Green New Deal', 'Biden', 'Cory Booker', 'Pete Buttigieg', 'Donald Trump', 'Russia', 'Sri Lanka', 'Notre Dame', 'Brexit', 'Easter', 'Conway', 'Wall']
+    keywords = ['Mueller', 'Election 2020', 'Indian Elections', 'Bernie Sanders', 'Trump',
+    'Green New Deal', 'Biden', 'Cory Booker', 'Pete Buttigieg', 'Donald Trump', 'Russia', 'Sri Lanka', 'Notre Dame', 'Brexit', 'Easter', 'Conway', 'Wall']
     for k in keywords:
-        random, v = top(k)
-        db_util.insertJSON('trendingTopics', v)
-        print(k + " added")
+        v = top(k)
+        # db_util.insertJSON('trendingTopics', v)
+        print(k, v)
 
 def addRunners():
     keywords = ['Cory Booker', 'Pete Buttigieg', 'Julian Castro', "John Delaney", "Tulsi Gabbard", "Kirsten Gillibrand", "Kamala Harris", "John Hickenlooper", 'Jay Inslee', 'Amy Klobuchar', 'Wayne Messam', 'Beto O\'Rourke', 'Bernie Sanders', 'Eric Swalwell', 'Donald Trump', 'Elizabeth Warren', 'Bill Weld', 'Marianne Williamson', "Andrew Yang"]
@@ -94,8 +95,7 @@ def checkSources():
     allSources = set(allSources)
     print(allSources)
     print("Size: ", len(allSources))
+
 # addTopHeadlines()
-
-
-# addSourceDistributions()
-addRunners()
+addSourceDistributions()
+# addRunners()
