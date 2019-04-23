02439d0b1bd2643e9086348cbccc570033d0c10c
Ashwin Natarajan <ashwinn3@gmail.com>
2019-04-22 22:10:58 -0400

    one more fix

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def index():
 
 @app.route('/getSourceDistribution/<key_word>', methods=['GET'])
 def getSourceDistribution(key_word):
-	data = read(key_word)
+	data = read(key_word)[1]
 	old = getJSON("sourceDistribution")
 	body = old['body']
 	duplicate = False
@@ -22,8 +22,14 @@ def getSourceDistribution(key_word):
 		if item['keyword'] == key_word:
 			duplicate = True
 	if not duplicate:
-		insertJSON("sourceDistribution", data)
-
+		dynamodb = boto3.resource('dynamodb',aws_access_key_id=config.aws_access_key_id, aws_secret_access_key=config.aws_secret_access_key, region_name='us-east-2')
+		table = dynamodb.Table("sourceDistribution")
+		table.put_item(
+			Item={
+				'keyword': key_word,
+				'publications': str(data['publications'])
+			})
+	
 	return jsonify(data)
 
 @app.route('/getTrending/', methods=['GET'])
